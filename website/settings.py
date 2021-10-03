52a5e989dfe84f1e1b7d4261dda058c6b3aa1ed2
ahmedsharifkhan <65096534+ahmedsharifkhan@users.noreply.github.com>
2021-10-04 04:36:50 +0600

    production setup

--- website/settings.py
+++ website/settings.py
@@ -5,13 +5,16 @@ import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j%glmpja4o7aeo=x9mj(o!pe4(uinexd(vb9p^e3ty8y)vw&ib'
 
+
+with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
+    SECRET_KEY = f.read().strip()
+
+# SECURE_HSTS_SECONDS = 31536000
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = os.environ.get('DEBUG', True)
@@ -32,6 +35,9 @@ INSTALLED_APPS = [
 
 
     'ckeditor',
+
+    'asymmetric_jwt_auth',
+    'certbot_django.server',
 ]
 
 MIDDLEWARE = [
@@ -46,6 +52,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'website.urls'
+# CSRF_COOKIE_SECURE = True
+# SECURE_SSL_REDIRECT = True
 
 TEMPLATES = [
     {
--- website/urls.py
+++ website/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import include, url
+import certbot_django.server.urls
 
 from django.conf.urls.static import static
 from django.conf import settings
@@ -7,6 +9,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin', admin.site.urls),
     path('', include('blog.urls')),
+    url(r'^\.well-known/', include(certbot_django.server.urls)),
+
     # path('employees/', include('employees.urls')),
 ]
 
