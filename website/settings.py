6f8caaaeafe4971e6a2785a4fa9dd3831c5d0a5e
ahmedsharifkhan <65096534+ahmedsharifkhan@users.noreply.github.com>
2021-10-04 03:18:41 +0600

    debug section edit

--- website/settings.py
+++ website/settings.py
@@ -13,7 +13,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j%glmpja4o7aeo=x9mj(o!pe4(uinexd(vb9p^e3ty8y)vw&ib'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DEBUG', True)
 
 ALLOWED_HOSTS = ['*']
 
